2c274880efeb72fe26d8f744ced77b991fc742b8
Zach Masson <zachmasson96@gmail.com>
2019-07-24 13:11:27 +0000

    homework update

--- Lesson 4 Exercises.R
+++ Lesson 4 Exercises.R
@@ -10,7 +10,16 @@ cbp_AP_23 <- fread('data/cbp15co.csv') %>% filter(NAICS == '23----') %>% select(
 ##^ select = takes all the values in the column, filter= takes what u wanna get
 
 ##3
+cbp_21_emp <- fread('data/cbp15co.csv', na.strings = '') %>%
+  filter(NAICS == '21----') %>% group_by(FIPSTATE, FIPSCTY) %>%
+  summarize(EMP = sum(EMP)) %>%
+  summarize(EMP = sum(EMP), counties = n())
 
-
+##4
+pivottable <- fread('data/cbp15co.csv', na.strings = '') %>%
+  filter(str_detect(NAICS, '[0-9]{2}----')) %>%
+  group_by(FIPSTATE, NAICS) %>%
+  summarize(EMP = sum(EMP)) %>%
+  spread(key = NAICS, value = EMP)
 
 
